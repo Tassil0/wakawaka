ed1b3b9ead3a658bc4f4a877dff8d3c5c07126ef
Filip Sikora <contact@fsikora.com>
2022-12-29 23:00:00 +0100

    animation and ghost types

--- src/types.h
+++ src/types.h
@@ -7,6 +7,20 @@
 #ifndef WAKAWAKA_TYPES_H
 #define WAKAWAKA_TYPES_H
 
+#include "config.h"
+#include <inttypes.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef float f32;
+typedef double f64;
+
 typedef struct {
     void (*logic)(void);
 
@@ -21,7 +35,15 @@ typedef struct {
 } App;
 
 // UNDF = UNDEFINED LMAO
-enum directions { LEFT, DOWN, UP, RIGHT, UNDF };
+enum Directions { LEFT, DOWN, UP, RIGHT, UNDF };
+
+typedef struct {
+    SDL_Point clip;
+    SDL_Point leftClips[4];
+    SDL_Point downClips[4];
+    SDL_Point upClips[4];
+    SDL_Point rightClips[4];
+} Animation;
 
 typedef struct {
     int w;
@@ -30,13 +52,36 @@ typedef struct {
     float y;
     float dx;
     float dy;
-    enum directions currMove;
-    enum directions nextMove;
+    enum Directions currMove;
+    enum Directions nextMove;
     SDL_Point center;
     SDL_Point gridPos;
     SDL_Rect rect;
     SDL_Texture *texture;
+    Animation animation;
 } PlayerEntity;
+
+typedef struct {
+    SDL_Point pos;
+    SDL_Point gridPos;
+    SDL_Rect hitbox;
+    enum Directions currMove;
+    SDL_Point target;
+    SDL_Texture *texture;
+    SDL_Point texturePos;
+    Animation animation;
+} GhostEntity;
+
+/*typedef enum { Frightened, Chase, Scatter } GhostSates;
+
+typedef struct GhostState *GhostStatePtr;
+typedef void (*EventStartFunc)(GhostStatePtr);
+typedef void (*EventStopFunc)(GhostStatePtr);
+
+struct GhostState {
+    EventStartFunc start;
+    EventStopFunc stop;
+};*/
 
 typedef struct {
     int width;
