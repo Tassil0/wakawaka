f43c4e0d779114fd0c569e8ca989ac5b450bd289
Filip Sikora <contact@fsikora.com>
2023-01-02 16:30:42 +0100

    add colors

---
+++ src/colors.h
@@ -0,0 +1,14 @@
+//
+// Created by tassilo on 1/2/23.
+//
+
+#ifndef WAKAWAKA_COLORS_H
+#define WAKAWAKA_COLORS_H
+
+#define RED   243, 139, 168
+#define PINK  245, 194, 231
+#define TEAL  148, 226, 213
+#define BLUE  137, 180, 250
+#define PEACH 250, 179, 135
+
+#endif   // WAKAWAKA_COLORS_H
