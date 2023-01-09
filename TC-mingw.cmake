999a6b900d42508658d97ea1af86ac4979c9d3b1
Filip Sikora <contact@fsikora.com>
2023-01-09 01:21:00 +0100

    github actions - windows

---
+++ TC-mingw.cmake
@@ -0,0 +1,12 @@
+set(CMAKE_SYSTEM_NAME Windows)
+
+set(CMAKE_C_COMPILER mingw-w64)
+
+
+# adjust the default behavior of the FIND_XXX() commands:
+# search programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# search headers and libraries in the target environment
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
